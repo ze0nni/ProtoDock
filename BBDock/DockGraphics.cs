7087502a52144d434462178912134e75e5ac559f
ze0nni <ze0nni@gmail.com>
2021-09-04 12:17:28 +0300

    Рисование панели

---
+++ BBDock/DockGraphics.cs
@@ -0,0 +1,138 @@
+﻿using BBDock.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BBDock
+{
+    public sealed class DockGraphics
+    {
+        public Bitmap Bitmap { get; private set; }
+        private Graphics _graphics;
+
+        private readonly int _iconSize;
+        private readonly int _iconSpace;
+        private readonly Padding _paddings;
+
+        private readonly Bitmap _skin;
+        private readonly Padding _skin9Scale;
+
+
+        public DockGraphics(
+            int iconSize,
+            int iconSpace,
+            Padding paddings,
+            Bitmap skin,
+            Padding skin9Scale
+        )
+        {
+            _iconSize = iconSize;
+            _iconSpace = iconSpace;
+            _paddings = paddings;
+
+            _skin = skin;
+            _skin9Scale = skin9Scale;
+        }
+
+        private Size CalculateSize()
+        {
+            var iconsCount = 5;
+            return new Size(
+                _paddings.Left + iconsCount * _iconSize + (iconsCount - 1) * _iconSpace + _paddings.Right,
+                _paddings.Top + _iconSize + _paddings.Bottom
+            );
+        }
+
+        internal void Render(Dock dock)
+        {
+            var size = CalculateSize();
+            if (Bitmap == null || Bitmap.Width != size.Width || Bitmap.Height != size.Height)
+            {
+                _graphics?.Dispose();
+                Bitmap?.Dispose();
+
+                Bitmap = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
+                _graphics = Graphics.FromImage(Bitmap);
+            }
+
+            _graphics.Clear(Color.Transparent);
+            RenderSkin(size);
+        }
+
+        private void RenderSkin(Size size)
+        {
+            var centerWidth = size.Width - _skin9Scale.Left - _skin9Scale.Right;
+            var centerHeight = size.Height - _skin9Scale.Top - _skin9Scale.Bottom;
+
+            var skinCenterWidth = _skin.Width - _skin9Scale.Left - _skin9Scale.Right;
+            var skinCenterHeight = _skin.Height - _skin9Scale.Top - _skin9Scale.Bottom;
+
+            //Top Left
+            _graphics.DrawImage(_skin,
+                new Rectangle(0, 0, _skin9Scale.Left, _skin9Scale.Top),
+                new Rectangle(0, 0, _skin9Scale.Left, _skin9Scale.Top),
+                GraphicsUnit.Pixel
+             );
+
+            //Top Middle
+            _graphics.DrawImage(_skin,
+                new Rectangle(_skin9Scale.Left, 0, centerWidth, _skin9Scale.Top),
+                new Rectangle(_skin9Scale.Left, 0, skinCenterWidth, _skin9Scale.Top),
+                GraphicsUnit.Pixel
+             );
+
+            //Top Right
+            _graphics.DrawImage(_skin,
+                new Rectangle(size.Width - _skin9Scale.Right, 0, _skin9Scale.Right, _skin9Scale.Top),
+                new Rectangle(_skin.Width - _skin9Scale.Right, 0, _skin9Scale.Right, _skin9Scale.Top),
+                GraphicsUnit.Pixel
+             );
+
+            //Center left
+            _graphics.DrawImage(_skin,
+                new Rectangle(0, _skin9Scale.Top, _skin9Scale.Left, centerHeight),
+                new Rectangle(0, _skin9Scale.Top, _skin9Scale.Left, skinCenterWidth),
+                GraphicsUnit.Pixel
+             );
+
+            //Center Middle
+            _graphics.DrawImage(_skin,
+                new Rectangle(_skin9Scale.Left, _skin9Scale.Top, centerWidth, centerHeight),
+                new Rectangle(_skin9Scale.Left, _skin9Scale.Top, skinCenterWidth, skinCenterHeight),
+                GraphicsUnit.Pixel
+             );
+
+            //Center Right
+            _graphics.DrawImage(_skin,
+                new Rectangle(size.Width - _skin9Scale.Right, _skin9Scale.Top, _skin9Scale.Right, centerHeight),
+                new Rectangle(_skin.Width - _skin9Scale.Right, _skin9Scale.Top, _skin9Scale.Right, skinCenterHeight),
+                GraphicsUnit.Pixel
+             );
+
+            //Bottom Left
+            _graphics.DrawImage(_skin,
+                new Rectangle(0, size.Height - _skin9Scale.Bottom, _skin9Scale.Left, _skin9Scale.Bottom),
+                new Rectangle(0, _skin.Height - _skin9Scale.Bottom, _skin9Scale.Left, _skin9Scale.Bottom),
+                GraphicsUnit.Pixel
+             );
+
+            //Bottom Middle
+            _graphics.DrawImage(_skin,
+                new Rectangle(_skin9Scale.Left, size.Height - _skin9Scale.Bottom, centerWidth, _skin9Scale.Bottom),
+                new Rectangle(_skin9Scale.Left, _skin.Height - _skin9Scale.Bottom, skinCenterWidth, _skin9Scale.Bottom),
+                GraphicsUnit.Pixel
+             );
+
+            //Bottom Right
+            _graphics.DrawImage(_skin,
+                new Rectangle(size.Width - _skin9Scale.Right, size.Height - _skin9Scale.Bottom, _skin9Scale.Right, _skin9Scale.Bottom),
+                new Rectangle(_skin.Width - _skin9Scale.Right, _skin.Height - _skin9Scale.Bottom, _skin9Scale.Right, _skin9Scale.Bottom),
+                GraphicsUnit.Pixel
+             );
+        }
+    }
+}
--- BBDock/DockWindow.cs
+++ BBDock/DockWindow.cs
@@ -1,17 +1,17 @@
 ﻿using BBDock.Core;
 using System;
-using System.Runtime.InteropServices;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace BBDock
 {
     public partial class DockWindow : Form
     {
-        public IntPtr HInstance => Marshal.GetHINSTANCE(GetType().Module);
 
-        private Dock _dock;
+        public IntPtr HInstance => User32.HInstance;
 
-        private int _iconsCount;
+        private readonly DockGraphics _graphics;
+        private readonly Dock _dock;        
 
         public DockWindow(): base()
         {
@@ -19,19 +19,17 @@ namespace BBDock
             this.ShowInTaskbar = false;
             this.ShowIcon = false;
             this.FormBorderStyle = FormBorderStyle.None;
-
+            
+            _graphics = new DockGraphics(
+                64,
+                8,
+                new Padding(16, 16, 16, 8),
+                new Bitmap(@"D:\Projects\BBDock\Assets\Panel.png"),
+                new Padding(32, 32, 32, 32)
+            );
             _dock = new Dock(this);
-        }
 
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == (int)PInvoke.User32.WindowMessage.WM_PAINT)
-            {
-                UpdatePosition();
-                return;
-            }
-
-            base.WndProc(ref m);
+            //TODO: _graphics.Dispose()
         }
 
         protected override CreateParams CreateParams
@@ -40,6 +38,7 @@ namespace BBDock
             {
                 var p = base.CreateParams;
                 p.ExStyle |= (int)PInvoke.User32.WindowStylesEx.WS_EX_TOOLWINDOW;
+                p.ExStyle |= (int)PInvoke.User32.WindowStylesEx.WS_EX_LAYERED;
                 return p;
             }
         }
@@ -48,7 +47,6 @@ namespace BBDock
 
         public void SetIconsCount(int count)
         {
-            _iconsCount = count;
             UpdatePosition();
         }
 
@@ -57,10 +55,47 @@ namespace BBDock
             var screen = Screen.AllScreens[0];
             var bounds = screen.Bounds;
 
-            this.Width = IconSize * _iconsCount;
-            this.Height = IconSize;
-            this.Left = (bounds.Width - this.Width) / 2;
-            this.Top = (bounds.Height - this.Height);
+            _graphics.Render(_dock);
+            SetImage(_graphics.Bitmap);
+
+            this.Left = (bounds.Width - _graphics.Bitmap.Width) / 2;
+            this.Top = (bounds.Height - _graphics.Bitmap.Height);
+        }
+
+        public void SetImage(Bitmap bitmap)
+        {
+             var screenDC = User32.GetDC(IntPtr.Zero);
+             var memDC = User32.CreateCompatibleDC(screenDC);
+
+            IntPtr bitmapHandle = IntPtr.Zero;
+            IntPtr oldBitmapHandle = IntPtr.Zero;
+            try
+            {
+                bitmapHandle = bitmap.GetHbitmap(Color.FromArgb(0));
+                oldBitmapHandle = User32.SelectObject(memDC, bitmapHandle);
+
+                var size = new User32.Size { cx = bitmap.Width, cy = bitmap.Height };
+                var poinSource = new User32.Point { x = 0, y = 0 };
+                var topPos = new User32.Point { x = this.Left, y = this.Top };
+                var blend = new User32.BLENDFUNCTION
+                {
+                    blendOp = User32.AC_SRC_ALPHA,
+                    blendFlags = 0,
+                    sourceConstantAlpha = 255,
+                    alphaFormat = User32.AC_SRC_ALPHA
+                };
+                User32.UpdateLayeredWindow(this.Handle, screenDC, ref topPos, ref size, memDC, ref poinSource, 0, ref blend, User32.ULW_ALPHA);
+            }
+            finally
+            {
+                User32.ReleaseDC(IntPtr.Zero, screenDC);
+                if (bitmapHandle != IntPtr.Zero)
+                {
+                    User32.SelectObject(memDC, oldBitmapHandle);
+                    User32.DeleteObject(bitmapHandle);
+                }
+                User32.DeleteDC(memDC);
+            }
         }
     }
 }
