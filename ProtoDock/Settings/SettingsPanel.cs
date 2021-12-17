e263224300f659260bac45847fdbe296076aada6
ze0nni <ze0nni@gmail.com>
2021-12-17 14:40:37 +0300

    Extract class for settings

---
+++ ProtoDock/Settings/SettingsPanel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using ProtoDock.Api;
+using Label = System.Windows.Forms.Label;
+using Panel = System.Windows.Forms.Panel;
+
+namespace ProtoDock.Settings {
+	internal sealed class SettingsPanel : IDockSettingsDisplay {
+		private readonly Core.Dock _dock;
+		private readonly Panel _content;
+		
+		private readonly List<(Label label, ISettingsLine line)> _lines = new List<(Label, ISettingsLine)>();
+
+		public SettingsPanel(Core.Dock dock, Panel content) {
+			_dock = dock;
+			_content = content;
+		}
+
+		public void Clear()
+		{
+			foreach (var line in _lines)
+			{
+				if (line.label != null)
+				{
+					_content.Controls.Remove(line.label);
+					line.label.Dispose();
+				}
+				_content.Controls.Remove(line.line.Control);
+				line.line.Control.Dispose();
+			}
+			_lines.Clear();
+		}
+		
+		private T Add<T>(string labelText, T line) where T : ISettingsLine
+		{
+
+			var left = 0;
+			var top = 8;
+			if (_lines.Count > 0)
+			{
+				var lastLine = _lines[_lines.Count - 1];
+				top = lastLine.line.Control.Top + lastLine.line.Control.Height + 8;
+			}
+
+
+			Label label = null;
+			if (!string.IsNullOrEmpty(labelText))
+			{
+				label = new Label();
+				label.AutoSize = false;
+				label.Width = 200;
+				label.Text = labelText;                
+				label.Top = top;
+				label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+
+				_content.Controls.Add(label);
+                
+				left += 200 + 8;
+			}
+
+			line.Control.Left = left;
+			line.Control.Top = top;
+			line.Control.Width = _content.Width - left;
+			line.Control.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right;
+
+			if (_lines.Count > 0)
+			{
+				var lastLine = _lines[_lines.Count - 1];
+				line.Control.Top = lastLine.line.Control.Top + lastLine.line.Control.Height + 8;
+			}
+
+			_lines.Add((label, line));
+			_content.Controls.Add(line.Control);
+
+			return line;
+		}
+
+		void IDockSettingsDisplay.Header(string text) {
+			Add(null, new SettingsHeader(text));
+		}
+
+		void IDockSettingsDisplay.Combo<T>(
+			string label,
+			T selected,
+			IEnumerable<T> items,
+			out ICollectionController<T> controller,
+			Action<T> onValueChanged) {
+			var c = new SettingsCombo<T>(selected, items, onValueChanged);
+			controller = c;
+
+			Add(label, c);
+		}
+
+		void IDockSettingsDisplay.List<T>(
+			string label,
+			T selected,
+			IEnumerable<T> items,
+			out ICollectionController<T> controller,
+			Action<T> onValueChanged) {
+			var c = new SettingsList<T>(selected, items, onValueChanged);
+			controller = c;
+
+			Add(label, c);
+		}
+
+		void IDockSettingsDisplay.Toggle(
+			string label,
+			bool value,
+			out Func<bool> getValue,
+			out Action<bool> setValue,
+			Action<bool> onValueChanged) {
+			getValue = default;
+			setValue = default;
+			Add(label, new SettingsToggle(value, out getValue, out setValue, onValueChanged));
+		}
+
+		void IDockSettingsDisplay.Number(
+			string label,
+			int value,
+			int min,
+			int max,
+			out Action<int> setValue,
+			Action<int> onValueChange) {
+			Add(label, new SettingsNumber(value, min, max, out setValue, onValueChange));
+		}
+
+		public IButtonsPanel Buttons() {
+			return Add(null, new SettingsButtonsPanel());
+		}
+		
+		        
+		void IDockSettingsDisplay.Flush() {
+			_dock.Flush();
+		}
+
+		void IDockSettingsDisplay.SetDirty() {
+			_dock.Graphics.SetDirty();
+		}
+        
+		void IDockSettingsDisplay.FlashWindow() {
+			var info = new PInvoke.User32.FLASHWINFO();
+			info.cbSize = Marshal.SizeOf(info);
+			info.hwnd = _content.TopLevelControl.Handle;
+			info.dwFlags = PInvoke.User32.FlashWindowFlags.FLASHW_ALL | PInvoke.User32.FlashWindowFlags.FLASHW_TIMER;
+			info.uCount = 3;
+			PInvoke.User32.FlashWindowEx(ref info);
+		}
+	}
+}
--- ProtoDock/SettingsWindow.cs
+++ ProtoDock/SettingsWindow.cs
@@ -11,14 +11,16 @@ using System.Diagnostics;
 
 namespace ProtoDock
 {
-    public partial class SettingsWindow : Form, IDockSettingsContext, IDockSettingsDisplay
+    public partial class SettingsWindow : Form, IDockSettingsContext
     {
         private readonly Dock _dock;
+        private SettingsPanel _panel;
 
         internal SettingsWindow(Dock dock) : base()
         {
             InitializeComponent();
             _dock = dock;
+            _panel = new SettingsPanel(dock, Content);
 
             Register(new DockViewSettings(_dock.Graphics));
             Register(new DockPanelsSettings(_dock));
@@ -42,148 +44,11 @@ namespace ProtoDock
             Category.Items.Add(source);
         }
 
-        private void Category_SelectedIndexChanged(object sender, System.EventArgs e)
-        {
-            Clear();
+        private void Category_SelectedIndexChanged(object sender, System.EventArgs e) {
+            _panel.Clear();
 
             var source = (IDockSettingsSource)Category.SelectedItem;            
-            source.Display(this);
-        }
-
-        private readonly List<(Label label, ISettingsLine line)> _lines = new List<(Label, ISettingsLine)>();
-
-        private void Clear()
-        {
-            foreach (var line in _lines)
-            {
-                if (line.label != null)
-                {
-                    Content.Controls.Remove(line.label);
-                    line.label.Dispose();
-                }
-                Content.Controls.Remove(line.line.Control);
-                line.line.Control.Dispose();
-            }
-            _lines.Clear();
-        }
-
-        private T Add<T>(string labelText, T line) where T : ISettingsLine
-        {
-
-            var left = 0;
-            var top = 8;
-            if (_lines.Count > 0)
-            {
-                var lastLine = _lines[_lines.Count - 1];
-                top = lastLine.line.Control.Top + lastLine.line.Control.Height + 8;
-            }
-
-
-            Label label = null;
-            if (!string.IsNullOrEmpty(labelText))
-            {
-                label = new Label();
-                label.AutoSize = false;
-                label.Width = 200;
-                label.Text = labelText;                
-                label.Top = top;
-                label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-
-                Content.Controls.Add(label);
-                
-                left += 200 + 8;
-            }
-
-            line.Control.Left = left;
-            line.Control.Top = top;
-            line.Control.Width = Content.Width - left;
-            line.Control.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right;
-
-            if (_lines.Count > 0)
-            {
-                var lastLine = _lines[_lines.Count - 1];
-                line.Control.Top = lastLine.line.Control.Top + lastLine.line.Control.Height + 8;
-            }
-
-            _lines.Add((label, line));
-            Content.Controls.Add(line.Control);
-
-            return line;
-        }
-
-        void IDockSettingsDisplay.Flush() {
-            _dock.Flush();
-        }
-
-        void IDockSettingsDisplay.SetDirty() {
-            _dock.Graphics.SetDirty();
-        }
-        
-        void IDockSettingsDisplay.FlashWindow() {
-            var info = new PInvoke.User32.FLASHWINFO();
-            info.cbSize = Marshal.SizeOf(info);
-            info.hwnd = Handle;
-            info.dwFlags = PInvoke.User32.FlashWindowFlags.FLASHW_ALL | PInvoke.User32.FlashWindowFlags.FLASHW_TIMER;
-            info.uCount = 3;
-            PInvoke.User32.FlashWindowEx(ref info);
-        }
-        
-        void IDockSettingsDisplay.Header(string text)
-        {
-            Add(null, new SettingsHeader(text));
-        }
-
-        void IDockSettingsDisplay.Combo<T>(
-            string label,
-            T selected,
-            IEnumerable<T> items,
-            out ICollectionController<T> controller,
-            Action<T> onValueChanged)
-        {
-            var c = new SettingsCombo<T>(selected, items, onValueChanged);
-            controller = c;
-
-            Add(label, c);
-        }
-        
-        void IDockSettingsDisplay.List<T>(
-            string label,
-            T selected,
-            IEnumerable<T> items,
-            out ICollectionController<T> controller,
-            Action<T> onValueChanged)
-        {
-            var c = new SettingsList<T>(selected, items, onValueChanged);
-            controller = c;
-
-            Add(label, c);
-        }
-
-        void IDockSettingsDisplay.Toggle(
-            string label,
-            bool value,
-            out Func<bool> getValue,
-            out Action<bool> setValue,
-            Action<bool> onValueChanged)
-        {
-            getValue = default;
-            setValue = default;
-            Add(label, new SettingsToggle(value, out getValue, out setValue, onValueChanged));
-        }
-
-        void IDockSettingsDisplay.Number(
-            string label,
-            int value,
-            int min,
-            int max,
-            out Action<int> setValue,
-            Action<int> onValueChange)
-        {
-            Add(label, new SettingsNumber(value, min, max, out setValue, onValueChange));
-        }
-
-        public IButtonsPanel Buttons() {
-            return Add(null, new SettingsButtonsPanel());
+            source.Display(_panel);
         }
     }
 }
